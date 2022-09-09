0c12ffc773163448c23eb0f3752432a1daa57c19
Khoirul Arif <masterirul@gmail.com>
2022-09-09 20:39:15 +0700

    add native bridge

--- app/pages/checkout/index.tsx
+++ app/pages/checkout/index.tsx
@@ -3,10 +3,30 @@ import ProgressBar from "@app/components/ProgressBar";
 import TopBar, { topBarStyles } from "@app/components/TopBar";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { useEffect, useMemo, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import DeviceInfo from "react-native-device-info";
 
 const CheckoutPage = () => {
+  const [uniqueId, setUniqueId] = useState("");
   const { goBack } = useNavigation();
+  const deviceId = useMemo(() => {
+    return DeviceInfo.getDeviceId();
+  }, []);
+  const brand = useMemo(() => {
+    return DeviceInfo.getBrand();
+  }, []);
+
+  const getAsync = () => {
+    DeviceInfo.getUniqueId().then((res) => {
+      setUniqueId(res);
+    });
+  };
+
+  useEffect(() => {
+    getAsync();
+  }, []);
+
   return (
     <Page>
       <TopBar>
@@ -17,8 +37,16 @@ const CheckoutPage = () => {
       </TopBar>
       <View style={Styles.content}>
         <View style={Styles.wrapper}>
+          <Text style={Styles.label}>Brand</Text>
+          <Text style={Styles.bold}>{brand}</Text>
+        </View>
+        <View style={Styles.wrapper}>
           <Text style={Styles.label}>Device ID</Text>
-          <Text style={Styles.bold}>asdasdasd</Text>
+          <Text style={Styles.bold}>{deviceId}</Text>
+        </View>
+        <View style={Styles.wrapper}>
+          <Text style={Styles.label}>Unique ID</Text>
+          <Text style={Styles.bold}>{uniqueId}</Text>
         </View>
         <View style={Styles.wrapper}>
           <Text style={Styles.label}>Progress Bar</Text>
